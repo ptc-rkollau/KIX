731ec29f663ec5eb86a84520dc90d8280f46f94a
Rackdoll <rackdoll@gmail.com>
2020-03-19 02:33:46 +0100

    UI Elements
    UI utility behaviours for easy KIX connection

---
+++ KIX/Scripts/ui/KIXButton.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[System.Serializable]
+public struct DataItem
+{
+    public string ID;
+    public string Data;
+}
+
+public class KIXButton : KIXDispatcher, IPointerDownHandler, IPointerUpHandler
+{
+    public string EVENT_TYPE = "";
+    public List<DataItem> data;
+
+    private Vector2 oldPos;
+    private float _dragThreshold = 10.0f;
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        oldPos = eventData.position;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        float l = Vector2.SqrMagnitude( oldPos - eventData.position );
+        if( l < _dragThreshold )
+            FireKIXEvent( new KIXEvent( EVENT_TYPE, ConvertDataToKIXData() ));
+    }
+
+
+
+    private KIXData ConvertDataToKIXData()
+    {
+        KIXData result = new KIXData();
+        if (data.Count == 1)
+        {
+            result.Value = data[0].Data;
+        }
+        else
+        {
+            result.Value = convertDataToDictionary();
+        }
+        return result;
+    }
+    private Dictionary<string, object> convertDataToDictionary()
+    {
+        var result = new Dictionary<string, object>();
+        for(int i = 0; i < data.Count; ++i)
+        {
+            result[data[i].ID] = data[i].Data;
+        }
+        return result;
+    }
+}
---
+++ KIX/Scripts/ui/KIXView.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[AddComponentMenu("[ KIX] /UI/KIXView")]
+public class KIXView : KIXListener
+{
+    public string EVENT_TYPE;
+    public string Data_MATCH = "";
+    public bool IsHiddenAtStart = false;
+
+    private CanvasGroup _cnvsGroup;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _cnvsGroup = gameObject.AddComponent<CanvasGroup>();
+        ToggleViewVisibility(!IsHiddenAtStart);
+        AddEventListener(EVENT_TYPE, OnKIXEvent);
+    }
+
+    private void OnKIXEvent(KIXEvent evt)
+    {
+        if( evt.Type == EVENT_TYPE )
+        {
+            ToggleViewVisibility(((string)evt.Data.Value == Data_MATCH));
+        }
+    }
+
+    private void ToggleViewVisibility( bool visible )
+    {
+        _cnvsGroup.alpha = visible ? 1.0f: 0.0f;
+        _cnvsGroup.blocksRaycasts = visible;
+    }
+}
